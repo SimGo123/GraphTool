eaa1a559a5b8a931ce6bc622a0181cb94adee6fa
SimGo123 <43723284+SimGo123@users.noreply.github.com>
2023-09-28 12:27:02 +0200

    Make use of rdfs library function

--- okamuraSeymourAlgo.js
+++ okamuraSeymourAlgo.js
@@ -188,7 +188,7 @@ class OkamuraSeymourAlgo extends Algorithm {
             let copyDummyGraph = dummyHelpGraph.getCopy();
             let sourceVertex = dummyHelpGraph.getVertexByNumber(source);
             let targetVertex = dummyHelpGraph.getVertexByNumber(dummyHelpGraph.targets[i]);
-            let visited = this.rightDepthFirstSearch(
+            let visited = rightDepthFirstSearch(
                 sourceVertex, targetVertex, copyDummyGraph, true);
             console.log('visited', visited);
             if (visited != null) {
@@ -255,10 +255,10 @@ class OkamuraSeymourAlgo extends Algorithm {
             let targetVertex = dummyHelpGraph.getVertexByNumber(dummyHelpGraph.targets[i]);
             let visited = null;
             if (copyDummyHelpGraph.getIncidentEdges(sourceVertex, true).length > 0) {
-                visited = this.rightDepthFirstSearch(
+                visited = rightDepthFirstSearch(
                     sourceVertex, targetVertex, copyDummyHelpGraph, false);
             } else {
-                visited = this.rightDepthFirstSearch(
+                visited = rightDepthFirstSearch(
                     targetVertex, sourceVertex, copyDummyHelpGraph, false);
             }
             console.log('visited', visited);
@@ -304,118 +304,6 @@ class OkamuraSeymourAlgo extends Algorithm {
         awayVec.x *= -1;
         awayVec.y *= -1;
         return changeVectorLength(awayVec, 50);
-    }
-
-    rightDepthFirstSearch(startVertex, endVertex, runGraph, orientUnorientedEdges) {
-        console.log("rightDepthFirstSearch");
-        runGraph.indexAllEdges();
-        let visited = [];
-        let stack = [];
-        let incidentEdges = runGraph.getIncidentEdges(startVertex, true);
-        if (incidentEdges.length == 0) {
-            console.error("incidentEdges.length == 0");
-            console.log('startVertex', startVertex);
-            return;
-        }
-        let incidentEdge = incidentEdges[0];
-        if (incidentEdge.v1nr == startVertex.number) {
-            incidentEdge.orientation = EdgeOrientation.NORMAL;
-        } else {
-            incidentEdge.orientation = EdgeOrientation.REVERSED;
-        }
-        stack.push(incidentEdge);
-        while (stack.length > 0) {
-            let entryEdge = stack.pop();
-            console.log('visiting ' + entryEdge.prt());
-            visited.push(entryEdge);
-            let vertex = null;
-            if (entryEdge.orientation == EdgeOrientation.NORMAL) {
-                vertex = runGraph.getVertexByNumber(entryEdge.v2nr);
-            } else if (entryEdge.orientation == EdgeOrientation.REVERSED) {
-                vertex = runGraph.getVertexByNumber(entryEdge.v1nr);
-            }
-            if (vertex == null) {
-                console.error("vertex == null");
-                return;
-            }
-            if (vertex.number == endVertex.number) {
-                console.log('Found target');
-                // Reconstruct path
-                let result = [];
-                let lastConfirmed = endVertex.number;
-                for (let i = visited.length - 1; i >= 0; i--) {
-                    let visitedEdge = visited[i];
-                    if (visitedEdge.getEndVertexNr() != lastConfirmed) {
-                        continue;
-                    }
-                    if (visitedEdge.v1nr == lastConfirmed) {
-                        result.push(visitedEdge);
-                        console.log('adding ' + visitedEdge.prt() + ' to result');
-                        lastConfirmed = visitedEdge.v2nr;
-                    } else if (visitedEdge.v2nr == lastConfirmed) {
-                        result.push(visitedEdge);
-                        console.log('adding ' + visitedEdge.prt() + ' to result');
-                        lastConfirmed = visitedEdge.v1nr;
-                    }
-                    if (lastConfirmed == startVertex.number) {
-                        console.log('breaking');
-                        break;
-                    }
-                }
-                return result;
-            }
-            if (entryEdge == null) {
-                console.error("entryEdge == null");
-                return;
-            }
-            let edgesRightOfEntryEdge = this.getEdgesRightOfEntryEdge(vertex, entryEdge, runGraph);
-            // Reverse order because stack is LIFO
-            edgesRightOfEntryEdge.reverse();
-            edgesRightOfEntryEdge.forEach(e => {
-                if (eqIndexOf(visited, e, true, true) == -1) {
-                    if (orientUnorientedEdges) {
-                        if (e.v1nr == vertex.number) {
-                            e.orientation = EdgeOrientation.NORMAL;
-                        } else {
-                            e.orientation = EdgeOrientation.REVERSED;
-                        }
-                    }
-                    stack.push(e);
-                }
-            });
-        }
-        return null;
-    }
-
-    getEdgesRightOfEntryEdge(vertex, entryEdge, runGraph) {
-        let entryEdgeAngle = getAngle(vertex, runGraph.getOtherVertex(entryEdge, vertex));
-        let incidentEdges = runGraph.getIncidentEdges(vertex, true);
-        incidentEdges.sort((e1, e2) => {
-            let otherVertex1 = runGraph.getOtherVertex(e1, vertex);
-            let otherVertex2 = runGraph.getOtherVertex(e2, vertex);
-            let angle1 = getAngle(vertex, otherVertex1);
-            let angle2 = getAngle(vertex, otherVertex2);
-            return angle2 - angle1;
-        });
-        let edgesRightOfEntryEdge = [];
-        let nextSmallerEdgeIndex = -1;
-        for (let i = 0; i < incidentEdges.length; i++) {
-            let currAngle = getAngle(vertex, runGraph.getOtherVertex(incidentEdges[i], vertex));
-            if (currAngle < entryEdgeAngle) {
-                nextSmallerEdgeIndex = i;
-                break;
-            }
-        }
-        if (nextSmallerEdgeIndex == -1) {
-            nextSmallerEdgeIndex = 0;
-        }
-        for (let i = nextSmallerEdgeIndex; i < incidentEdges.length; i++) {
-            edgesRightOfEntryEdge.push(incidentEdges[i]);
-        }
-        for (let i = 0; i < nextSmallerEdgeIndex; i++) {
-            edgesRightOfEntryEdge.push(incidentEdges[i]);
-        }
-        return edgesRightOfEntryEdge;
     }
 }
 
